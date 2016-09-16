424040690eba031c10103b64cd7ef8cd748c03ab
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-09-16 18:09:29 +0900

    [Create] CollectionViewSample

---
+++ Example/UICollectionView/CollectionViewSample/CollectionViewSample/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CollectionViewSample
+//
+//  Created by ShinokiRyosei on 2016/09/16.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
